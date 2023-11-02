74da1c9b937560998a76213296bd31ac86b37a18
vaibhav <vaibhavmasetty06@gmail.com>
2023-11-02 17:31:09 -0400

    Adding 3Sum.java

--- ReverseWordsInAStringIII.java
+++ ReverseWordsInAStringIII.java
@@ -3,28 +3,15 @@ import java.util.Arrays;
 public class ReverseWordsInAStringIII {
     public static String reverseWords(String words){
         String[] word = words.split(" ");
+        StringBuilder reversed = new StringBuilder();
 
         for (int i=0; i<word.length; i++){
-            int m=0;
-            int n = word[i].length()-1;
-            char[] inp = word[i].toCharArray();
-            while (m<n){
-                char ch = inp[m];
-                inp[m] = inp[n];
-                inp[n] = ch;
-                m++;
-                n--;
-            }
-            word[i] = new String(inp);
+            StringBuilder reverseWord = new StringBuilder(word[i]);
+            reverseWord.reverse();
+            reversed.append(reverseWord).append(" ");
         }
-        String out = "";
-        for (int p=0; p<word.length; p++){
-            out += word[p];
-            if(p!=word.length-1){
-                out += " ";
-            }
-        }
-        return out;
+        return reversed.toString().trim();
+
     }
     public static void main(String[] args) {
         String word = "Let's take LeetCode contest";
